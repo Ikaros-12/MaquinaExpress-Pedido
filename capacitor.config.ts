44c8698c91f42749fb8159c75fbcc34f53de5aa5
Ikaros-12 <matias.oviedo94@gmail.com>
2024-11-18 23:58:12 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'MaquinaExpress-Pedido',
+  webDir: 'www'
+};
+
+export default config;
